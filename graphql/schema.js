9d90111b625b46cdf38f8ae5df18aa4a44282a28
FerdinandYetna <ferdiyetna@gmail.com>
2021-06-12 02:35:17 +0200

    first commit

---
+++ graphql/schema.js
@@ -0,0 +1,42 @@
+const { GraphQLSchema, GraphQLObjectType } = require("graphql")
+
+
+const { users, user, posts, post, postByUser,comments, comment } = require("./queries")
+
+const {
+  register,
+  login,
+  addPost,
+  addComment,
+  updatePost,
+  deletePost,
+  updateComment,
+  deleteComment,
+} = require("./mutations")
+
+const QueryType = new GraphQLObjectType({
+  name: "QueryType",
+  description: "Queries",
+  fields: { users, user, posts, post,postByUser, comments, comment },
+})
+
+
+const MutationType = new GraphQLObjectType({
+  name: "MutationType",
+  description: "Mutations",
+  fields: {
+    register,
+    login,
+    addPost,
+    addComment,
+    updatePost,
+    deletePost,
+    updateComment,
+    deleteComment,
+  },
+})
+
+module.exports = new GraphQLSchema({
+  query: QueryType,
+  mutation: MutationType,
+})
